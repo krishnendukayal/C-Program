b6814f66d49c41edb0dee25a24a670ed9e525a89
krishnendu kayal <krishnendu1985@gmail.com>
2024-01-03 13:29:05 +0530

    for c program

--- main.c
+++ main.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 int main() {
    // printf() displays the string inside quotation
-   printf("Hello, World!");
+   printf("Hello, World!");// printf() displays the string inside quotation
+   printf("Hello, World!");// printf() displays the string inside quotation
+   printf("Hello, World!");// printf() displays the string inside quotation
    return 0;
 }
